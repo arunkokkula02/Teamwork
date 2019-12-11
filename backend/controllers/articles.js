aaee6637eca8626c8860dca92e6e06f0c1951023
caleb <caleb.geek.gk@gmail.com>
2019-12-11 19:59:47 +0300

    Adding comment functionality

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -20,6 +20,17 @@ exports.getArticles= (request, response) => {
     })
   }
 
+//Function to view a specific article
+exports.getArticleById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    db.query('SELECT * FROM articles WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
 
 //Function for editing an existing or specific article
 exports.updateArticle = (request, response) => {
@@ -43,6 +54,19 @@ exports.updateArticle = (request, response) => {
       if (error) {
         throw error
       }
-      response.status(200).json(`User deleted with ID: ${id}`)
+      response.status(200).json(`Article deleted with ID: ${id}`)
     })
+  }
+
+  //Function to comment on article
+  exports.commentOnArticle = (req,res) => {
+    const article_id = parseInt(req.params.id)
+    const {comment,user_id} = req.body
+    const created_on = new Date()
+    db.query('INSERT INTO comments (comment,created_on,user_id,article_id) VALUES ($1, $2, $3, $4)', [comment,created_on,user_id,article_id], (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.status(200).json(`Comment posted with ID: ${article_id}`)
+      })
   }
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -4,8 +4,10 @@ const articlesCtrl = require('../controllers/articles');
 const router = express.Router();
 router.post('/', articlesCtrl.createArticle);
 router.get('/', articlesCtrl.getArticles);
+router.get('/:id', articlesCtrl.getArticleById);
 router.put('/:id', articlesCtrl.updateArticle);
 router.delete('/:id', articlesCtrl.deleteArticle);
+router.post('/:id/comment',articlesCtrl.commentOnArticle);
 
 
 
